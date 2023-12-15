7b0cac74204b7313c56e03c95d51223147c06161
kailash <kailashrajut0575@gmail.com>
2023-12-15 21:22:10 +0530

    Wc-1

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Counter } from "./features/counter/Counter";
+
+function App() {
+  return (
+    <>
+      <h1>Counter</h1>
+      <Counter/>
+    </>
+  );
+}
+
+export default App;
