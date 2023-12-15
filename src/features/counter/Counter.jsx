c1d17430e38681cef67a61440683a98503d89389
kailash <kailashrajut0575@gmail.com>
2023-12-15 22:00:42 +0530

    let begin project

--- src/features/counter/Counter.jsx
+++ src/features/counter/Counter.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { decrement, increment ,incrementByTwo ,decrementByTwo } from "./counterSlice";
+import {
+  decrement,
+  increment,
+  incrementByTwo,
+  decrementByTwo,
+} from "./counterSlice";
 
 export function Counter() {
   const count = useSelector((state) => state.counter.value);
@@ -11,28 +16,12 @@ export function Counter() {
     <div>
       <div>
         <h1>{count}</h1>
-        <button
-          
-          onClick={() => dispatch(increment())}
-        >
-          Increment
-        </button>
-        <button
-          
-          onClick={() => dispatch(decrement())}
-        >
-          Decrement
-        </button>
-        <button
-      
-          onClick={() => dispatch(incrementByTwo())}
-        >
+        <button onClick={() => dispatch(increment())}>Increment</button>
+        <button onClick={() => dispatch(decrement())}>Decrement</button>
+        <button onClick={() => dispatch(incrementByTwo())}>
           Increment By Two
         </button>
-        <button
-          
-          onClick={() => dispatch(decrementByTwo())}
-        >
+        <button onClick={() => dispatch(decrementByTwo())}>
           Decrement By Two
         </button>
       </div>
